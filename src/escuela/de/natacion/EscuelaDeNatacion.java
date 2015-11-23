e9b941f39ec9dea1ab39dad77d4268ade83e17fb
membrilloIsaac <guillermomembri@gmail.com>
2015-11-23 03:32:24 -0600

    Sincronizar

---
+++ src/escuela/de/natacion/EscuelaDeNatacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escuela.de.natacion;
+
+/**
+ *
+ * @author Guillermo
+ */
+public class EscuelaDeNatacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login v=new Login();
+       v.setVisible(true);
+    }
+    
+}
