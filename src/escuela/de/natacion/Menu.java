295653b5358b36ced592aa2292cf4b610d4c4883
Rigo Bono <arigoberto@gmail.com>
2015-11-25 20:50:02 -0600

    Aplicacion de nuevas opciones de promocion a menu

--- src/escuela/de/natacion/Menu.java
+++ src/escuela/de/natacion/Menu.java
@@ -53,6 +53,7 @@ public class Menu extends javax.swing.JFrame {
         MenuRegistrarPago = new javax.swing.JMenuItem();
         MenuBuscarFolio = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -226,13 +227,21 @@ public class Menu extends javax.swing.JFrame {
         });
         MenuPago.add(MenuBuscarFolio);
 
-        jMenuItem2.setText("Promocion");
+        jMenuItem2.setText("Añadir Promocion");
         jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem2ActionPerformed(evt);
             }
         });
         MenuPago.add(jMenuItem2);
+
+        jMenuItem3.setText("Manejar promocion");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        MenuPago.add(jMenuItem3);
 
         jMenuBar1.add(MenuPago);
 
@@ -373,6 +382,12 @@ public class Menu extends javax.swing.JFrame {
         a.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        ManejaPromo mp=new ManejaPromo();
+        mp.setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -433,6 +448,7 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem5;
     // End of variables declaration//GEN-END:variables
 }
